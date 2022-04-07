87c5c3cd11c2d52b5202a657c156af7e3e20c7e0
lghiggino <lghiggino@gmail.com>
2022-04-07 15:44:43 -0300

    função de parse e uso no teste

--- src/cnabParser.js
+++ src/cnabParser.js
@@ -1,4 +1,23 @@
-function sum(a, b) {
-    return a + b;
+function cnabParser(string) {
+    const tipo = string.slice(0, 1)
+    const data = string.slice(1, 9)
+    const valor = string.slice(9, 19)
+    const cpf = string.slice(19, 30)
+    const cartao = string.slice(30, 46)
+    const hora = string.slice(46, 52)
+    const donoDaLoja = string.slice(52, 65).trim()
+    const nomeDaLoja = string.slice(65, 80).trim()
+
+    return {
+        tipo,
+        data,
+        valor,
+        cpf,
+        cartao,
+        hora,
+        donoDaLoja,
+        nomeDaLoja
+    }
 }
-module.exports = sum;
+
+module.exports = cnabParser
--- test/cnabParser.test.js
+++ test/cnabParser.test.js
@@ -1,38 +1,29 @@
-const sum = require('../src/cnabParser');
+const cnabParser = require("../src/cnabParser")
 const fs = require("fs");
 const path = require("path");
+const normalizeText = require("normalize-text")
 
 const file = path.join(__dirname, "../test/fixtures", "CNAB.txt");
-const fdr = fs.readFileSync(file, "utf8", function(err, data) {
-  return data;
+const fdr = fs.readFileSync(file, "utf8", function (err, data) {
+    return data;
 });
 
 const string = "3201903010000014200096206760174753****3153153453JOÃO MACEDO   BAR DO JOÃO       "
+const string2 = "5201903010000013200556418150633123****7687145607MARIA JOSEFINALOJA DO Ó - MATRIZ"
 
 
-expect(fdr).toBe(string)
+describe('Line By Line', () => {
 
-test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
-});
-
-test("cnab", () => {
-    expect(cnabTxt).toBe(true)
-})
-
-describe('DcoService', () => {
-
-    beforeEach(() => {
-        jest.spyOn(HttpClient, 'get').mockImplementation((_url, _options) => {
-            const data = require("../test/fixtures/CNAB.txt")
-            return Promise.resolve(data)
-        })
-    })
-
-    it('lista os dados da dco', async () => {
-        const today = dayjs().format('YYYY-MM-DD')
-        const dcoId = 1
-        const res = await DcoService.getVendasDeptosTotal(today, today, dcoId)
+    it('should make understable data out of a single line on the txt file', () => {
+        const parsed = cnabParser(string)
+        expect(parsed.tipo).toBe("3")
+        expect(parsed.data).toBe("20190301")
+        expect(parsed.valor).toBe("0000014200")
+        expect(parsed.cpf).toBe("09620676017")
+        expect(parsed.cartao).toBe("4753****31531534")
+        expect(parsed.hora).toBe("53    ")
+        expect(parsed.donoDaLoja).toBe("JOÃO MACEDO")
+        expect(parsed.nomeDaLoja).toBe("BAR DO JOÃO")
     })
 
 })
